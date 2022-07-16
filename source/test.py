8fc1328a565c9344e8e0ba1968a3e3e0c8d70f05
Andrey Kruglov <altos047@gmail.com>
2022-07-17 02:51:07 +0500

    commit

--- source/test.py
+++ source/test.py
@@ -1,16 +1,18 @@
-import pygame
+import os
 import sys
 
+import pygame
+
 pygame.init()
-window = pygame.display.set_mode((1200, 800))
-pygame.display.set_caption('Название игры!')
+window = pygame.display.set_mode((1400, 600))
+pygame.display.set_caption('REVIZOR')
 
-player = pygame.image.load('pers_1.png')
-background = pygame.image.load('back.jpg')
+player = pygame.image.load(os.path.join('data', 'pers_2.png'))
+#  background = pygame.image.load(os.path.join('data', 'back.jpg'))
 
-x = 10
-y = 10
-speed = 5
+x = 0
+y = 0
+speed = 20
 
 clock = pygame.time.Clock()
 run = True
@@ -21,9 +23,9 @@ while(run):
             run = False
             pygame.quit()
             sys.exit()
-    
+
     keys = pygame.key.get_pressed()
-    
+
     if keys[pygame.K_LEFT]:
         x -= speed
     elif keys[pygame.K_RIGHT]:
@@ -33,7 +35,8 @@ while(run):
     elif keys[pygame.K_DOWN]:
         y += speed
 
-    window.blit(background, (0, 0))
+    #  window.blit(background, (0, 0))
+    window.fill((0, 0, 0))
     window.blit(player, (x, y))
     pygame.display.update()
 pygame.quit()
